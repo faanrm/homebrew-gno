d47be542da97cae24a62cc4ebbba024689460657
faanrm <faniloniainaa21@gmail.com>
2024-02-07 22:42:05 +0300

    fix formula

--- Formula/gno.rb
+++ Formula/gno.rb
@@ -1,4 +1,4 @@
-class gno < Formula
+class Gno < Formula
     desc "Description of your package"
     homepage "https://github.com/faanrm/go-node"
     url "https://github.com/faanrm/go-node/releases/download/v0.0.1/gno_v0.0.1_linux_386.zip"
